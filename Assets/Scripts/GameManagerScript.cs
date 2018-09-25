4e63133b89289c9585af80b50aaad5580a080676
Shubham Tyagi <styagi.2301@gmail.com>
2018-09-26 00:13:24 +0530

    Added TileStyleSheet and TileScript
    TileStyleSheet covers all the numbers and colors of image and number.

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -8,7 +8,9 @@ public class GameManagerScript : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        TileScript[] allTilesOnDimention = FindObjectsOfType<TileScript>();
+        foreach (TileScript tile in allTilesOnDimention)
+            tile.Number = 0;
     }
 
     // Update is called once per frame
