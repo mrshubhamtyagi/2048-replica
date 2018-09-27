5f31007fc2ab1bc3b5e04822df38c3b1e5cdf1a6
Shubham Tyagi <styagi.2301@gmail.com>
2018-09-27 23:26:06 +0530

    Minor changes

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManagerScript : MonoBehaviour
 {
     private TileScript[,] allTiles = new TileScript[4, 4];
+    private List<TileScript> tileList = new List<TileScript>();
 
     // Use this for initialization
     void Start()
@@ -14,13 +15,31 @@ public class GameManagerScript : MonoBehaviour
         {
             tile.Number = 0;
             allTiles[tile.indexRow, tile.indexCol] = tile;
+            tileList.Add(tile);
+        }
+    }
+
+    // ----------------------------------------- Generating New Tile With a number either 2 or 4
+    private void GenerateTile()
+    {
+        if (tileList.Count > 0)
+        {
+            // pick index for random number tile;
+            int indexForNewNumber = Random.Range(0, tileList.Count);
+            int randomNumber = Random.Range(0, 10);
+            if (randomNumber == 0)
+                tileList[indexForNewNumber].Number = 4; // assign 4 (only 10% chances)
+            else
+                tileList[indexForNewNumber].Number = 2;
+            tileList.RemoveAt(indexForNewNumber);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Space))
+            GenerateTile();
     }
 
     public void Move(MoveDirection direction)
